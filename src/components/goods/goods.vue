d5e2969fadd64f3e213676e6b56032debc8c8c5a
zxza <841827568@qq.com>
2017-11-30 10:41:39 +0800

    vue-route修改

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        I am goods
+    </div>
+</template>
+
+<script type="text/javascript">
+    export default {
+
+    }
+</script>
+
+<style lang="stylus" rel='stylesheet/stylus'>
+
+</style>
---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        I am ratings
+    </div>
+</template>
+
+<script type="text/javascript">
+    export default {
+
+    }
+</script>
+
+<style lang="stylus" rel='stylesheet/stylus'>
+
+</style>
