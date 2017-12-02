db014edb3c52f2c7d667fc6ca17aed5666b5b2b2
zxza <841827568@qq.com>
2017-12-02 23:58:59 +0800

    头部修改

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="goods">
       <div class='menu-wrapper'>
+        <ul>
+          <li v-for='item in goods' class="menu-item">
+            <span class='text border-1px'>
+              <span v-show='item.type>0' class="icon" :class='classMap[item.type]'></span>{{item.name}}
+            </span>
+          </li>
+        </ul>
       </div>
       <div class="foods-wrapper">
       </div>
@@ -23,11 +30,11 @@
         }
       },
       created() {
+        this.classMap = ['decrease','discount','special','invoice','guarantee'];
         this.$http.get('/api/goods').then((response) => {
           response = response.body;
           if(response.errno === ERR_OK){
               this.goods = response.data
-              console.log(this.goods)
           }
         })
       }
@@ -35,6 +42,9 @@
 </script>
 
 <style lang="stylus" rel='stylesheet/stylus'>
+
+    @import "../../common/stylus/mixin.styl"
+
     .goods
       display: flex
       position: absolute
@@ -46,6 +56,35 @@
         flex: 0 0 80px
         width: 80px
         background: #f3f5f7
+        .menu-item
+          display: table
+          height: 54px
+          width：56px
+          line-height: 14px
+          .icon
+            display: inline-block
+            vertical-align: top
+            width: 12px
+            height: 12px
+            margin-right: 2px
+            background-size: 12px 12px
+            background-repeat: no-repeat
+            &.decrease
+              bg-image('decrease_3')
+            &.discount
+              bg-image('discount_3')
+            &.guarantee
+              bg-image('guarantee_3')
+            &.invoice
+              bg-image('invoice_3')
+            &.special
+              bg-image('special_3')
+          .text
+            display: table-cell
+            width: 56px
+            vertical-align: middle
+            border-1px(rgba(7,17,27,0.1))
+            font-size: 12px
       .foods-wrapper
         flex: 1
 </style>
