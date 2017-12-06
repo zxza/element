75760e97230f252a3aa601cff022a80c6f74f9f3
zxza <841827568@qq.com>
2017-12-06 15:47:23 +0800

    split组件修改

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,6 +1,6 @@
-<template lang="html">
+<template>
   <transition name='move'>
-  <div class="food" v-show='showFlag' ref=food>
+  <div class="food" v-show='showFlag' ref="food">
     <div class="food-content">
       <div class="image-header">
         <img :src="food.image">
@@ -8,32 +8,45 @@
           <i class="icon-arrow_lift"></i>
         </div>
       </div>
-    </div>
-    <div class="content">
-      <h1 class="title">{{food.name}}</h1>
-      <div class="detail">
-        <span class="sell-count">月售{{food.sellCount}}</span>
-        <span class="rating">好评率{{food.rating}}</span>
-      </div>
-      <div class="price">
-        <span class="now">¥{{food.price}}</span><span class="old" v-show='food.oldPrice'>¥{{food.oldPrice}}</span>
-      </div>
-      <div class="cartcontrol-wrapper">
-        <cartcontrol :food='food'></cartcontrol>
-      </div>
-      <div class="buy" v-show="!food.count || food.count === 0">
-        加入购物车
-      </div>
-    </div>
-
+      <div class="content">
+        <h1 class="title">{{food.name}}</h1>
+        <div class="detail">
+          <span class="sell-count">月售{{food.sellCount}}</span>
+          <span class="rating">好评率{{food.rating}}</span>
+        </div>
+        <div class="price">
+          <span class="now">¥{{food.price}}</span><span class="old" v-show='food.oldPrice'>¥{{food.oldPrice}}</span>
+        </div>
+        <div class="cartcontrol-wrapper">
+          <cartcontrol :food='food'></cartcontrol>
+        </div>
+        <transition name='fade'>
+          <div @click.stop.prevent='addFirst($event)' class="buy" v-show="!food.count || food.count === 0">
+            加入购物车
+          </div>
+        </transition>
+       </div>
+       <split v-show='food.info'></split>
+       <div class="info" v-show='food.info'>
+         <h1 class="title">商品信息</h1>
+         <p class="text">{{food.info}}</p>
+       </div>
+       <split></split>
+       <div class="rating">
+         <h1 class="title">商品评价</h1>
+         <ratingselect></ratingselect>
+       </div>
   </div>
-
+</div>
 </transition>
 </template>
 
-<script>
+<script type="text/javascript">
 import BScroll from 'better-scroll';
-import cartcontrol from 'components/carcontrol/carcontrol'
+import Vue from 'vue';
+import cartcontrol from 'components/carcontrol/carcontrol';
+import split from 'components/split/split';
+import ratingselect from 'components/ratingselect/ratingselect';
 export default {
   props: {
     food: {
@@ -45,25 +58,37 @@ export default {
       showFlag: false
     };
   },
+  created() {
+    this.$nextTick(()=>{
+      if(!this.scroll) {
+        this.scroll = new BScroll(this.$refs.food,{
+          click: true
+        });
+        console.log(this.scroll)
+    } else {
+      this.scroll.refresh()
+    }
+  });
+},
   methods: {
     show() {
       this.showFlag = true;
-      this.$nextTick(()=>{
-        if(!this.scroll){
-          this.scroll = new BScroll(this.$refs.food,{
-            click: true
-          });
-      } else {
-        this.scroll.refresh()
-      }
-    })
     },
     hide() {
       this.showFlag = false;
+    },
+    addFirst(event) {
+      if(!event._constructed){
+        return;
+      }
+      this.$emit('add',event.target);
+      Vue.set(this.food,'count',1);
     }
   },
   components: {
-    cartcontrol
+    cartcontrol,
+    split,
+    ratingselect
   }
 }
 </script>
@@ -148,5 +173,23 @@ export default {
       border-radius: 12px
       font-size: 10px
       color: #fff
-      background: rgb(0,160,220)
+      background: rgb(0, 160, 220)
+      opacity: 1
+      &.fade-enter-active, &.fade-leave-active
+        transition: all 0.2s
+      &.fade-enter, &.fade-leave-active
+        opacity: 0
+        z-index: -1
+    .info
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 6px
+        font-size: 14px
+        color: rgb(7,17,27)
+      .text
+        line-height: 24px
+        padding: 0 8px
+        font-size: 12px
+        color: rgb(77,85,93)
 </style>
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -38,7 +38,7 @@
         </ul>
       </div>
       <shopcart ref='shopcart' :select-foods='selectFoods' :delivery-price='seller.deliveryPrice' :min-price='seller.minPrice'></shopcart>
-      <food :food='selectedFood' ref=food></food>
+      <food @add='addFood' :food='selectedFood' ref='food'></food>
     </div>
 </template>
 
